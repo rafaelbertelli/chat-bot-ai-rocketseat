785ecd3f37b10a9b2e967669b9bef7bc27b5e5e3
Rafael Borges <rafael.borges@luizalabs.com>
2023-07-19 22:48:13 -0300

    use api return

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -14,53 +14,57 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { useChat } from "ai/react";
 
 export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    api: "/api/chat",
+  });
+
   return (
-    <Card className="w-[440px] h-[700px] grid grid-rows-[min-content_1fr_min-content] ">
+    <Card className="w-[440px]">
       <CardHeader>
         <CardTitle>Chat AI</CardTitle>
         <CardDescription>Using Vercel SDK to create a chatbot</CardDescription>
       </CardHeader>
-      <CardContent className="space-y-4">
-        <CardContentInner>
-          <Avatar>
-            <AvatarFallback>RB</AvatarFallback>
-            <AvatarImage src="https://github.com/rafaelbertelli.png" />
-          </Avatar>
+      <CardContent>
+        <ScrollArea className="h-[600px] w-full space-y-4 pr-4">
+          {messages.map((m) => (
+            <CardContentInner key={m.id}>
+              <Avatar>
+                <AvatarFallback>
+                  {m.role === "user" ? "RB" : "AI"}
+                </AvatarFallback>
+                <AvatarImage
+                  src={
+                    m.role === "user"
+                      ? "https://github.com/rafaelbertelli.png"
+                      : "https://github.com/rocketseat.png"
+                  }
+                />
+              </Avatar>
 
-          <CardContentInnerParagraph>
-            <CardContentInnerParagraphDetached>
-              Human:
-            </CardContentInnerParagraphDetached>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores,
-            tempore. Unde optio quos error est aliquid consectetur, nobis
-            consequatur, mollitia, beatae tenetur repellendus nihil soluta iure
-            necessitatibus. Vitae, provident corporis!
-          </CardContentInnerParagraph>
-        </CardContentInner>
-
-        <CardContentInner>
-          <Avatar>
-            <AvatarFallback>AI</AvatarFallback>
-            <AvatarImage src="https://github.com/rocketseat.png" />
-          </Avatar>
-
-          <CardContentInnerParagraph>
-            <CardContentInnerParagraphDetached>
-              BOT:
-            </CardContentInnerParagraphDetached>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores,
-            tempore. Unde optio quos error est aliquid consectetur, nobis
-            consequatur, mollitia, beatae tenetur repellendus nihil soluta iure
-            necessitatibus. Vitae, provident corporis!
-          </CardContentInnerParagraph>
-        </CardContentInner>
+              <CardContentInnerParagraph>
+                <CardContentInnerParagraphDetached>
+                  {m.role === "user" ? "User:" : "Bot:"}
+                </CardContentInnerParagraphDetached>
+                {m.content}
+              </CardContentInnerParagraph>
+            </CardContentInner>
+          ))}
+        </ScrollArea>
       </CardContent>
 
-      <CardFooter className="space-x-2">
-        <Input placeholder="How can I help you?" />
-        <Button type="submit">Send</Button>
+      <CardFooter>
+        <form className="flex w-full gap-2" onSubmit={handleSubmit}>
+          <Input
+            placeholder="How can I help you?"
+            value={input}
+            onChange={handleInputChange}
+          />
+          <Button type="submit">Send</Button>
+        </form>
       </CardFooter>
     </Card>
   );
