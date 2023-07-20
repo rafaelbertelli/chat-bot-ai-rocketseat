0aa6cd626346615a99d2a8b997b1627f6c8432d1
Rafael Borges <rafael.borges@luizalabs.com>
2023-07-19 22:00:21 -0300

    abstract chat component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,9 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardContentInner,
-  CardContentInnerParagraph,
-  CardContentInnerParagraphDetached,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
+import Chat from "@/components/Chat";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-slate-50">
-      <Card className="w-[440px] h-[700px] grid grid-rows-[min-content_1fr_min-content] ">
-        <CardHeader>
-          <CardTitle>Chat AI</CardTitle>
-          <CardDescription>
-            Using Vercel SDK to create a chatbot
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="space-y-4">
-          <CardContentInner>
-            <Avatar>
-              <AvatarFallback>RB</AvatarFallback>
-              <AvatarImage src="https://github.com/rafaelbertelli.png" />
-            </Avatar>
-
-            <CardContentInnerParagraph>
-              <CardContentInnerParagraphDetached>
-                Human:
-              </CardContentInnerParagraphDetached>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores,
-              tempore. Unde optio quos error est aliquid consectetur, nobis
-              consequatur, mollitia, beatae tenetur repellendus nihil soluta
-              iure necessitatibus. Vitae, provident corporis!
-            </CardContentInnerParagraph>
-          </CardContentInner>
-
-          <CardContentInner>
-            <Avatar>
-              <AvatarFallback>AI</AvatarFallback>
-              <AvatarImage src="https://github.com/rocketseat.png" />
-            </Avatar>
-
-            <CardContentInnerParagraph>
-              <CardContentInnerParagraphDetached>
-                BOT:
-              </CardContentInnerParagraphDetached>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores,
-              tempore. Unde optio quos error est aliquid consectetur, nobis
-              consequatur, mollitia, beatae tenetur repellendus nihil soluta
-              iure necessitatibus. Vitae, provident corporis!
-            </CardContentInnerParagraph>
-          </CardContentInner>
-        </CardContent>
-
-        <CardFooter className="space-x-2">
-          <Input placeholder="How can I help you?" />
-          <Button type="submit">Send</Button>
-        </CardFooter>
-      </Card>
+      <Chat />
     </div>
   );
 }
